e09c40d1459072ee07c3b300dfbec25a1d82c9e0
mmsatark <mmsatark@ncsu.edu>
2019-01-15 18:39:04 -0800

    Update HR-IntPrep-Arrays-3.cs

--- HR-IntPrep-Arrays-3.cs
+++ HR-IntPrep-Arrays-3.cs
@@ -1,3 +1,7 @@
+/**
+https://www.hackerrank.com/challenges/new-year-chaos/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+**/
+
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Collections;
