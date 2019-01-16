ca5be02017c5be15eb6e62558e9a51d4a1ab9383
mmsatark <mmsatark@ncsu.edu>
2019-01-15 18:38:19 -0800

    Create HR-IntPrep-Arrays-3.cs

---
+++ HR-IntPrep-Arrays-3.cs
@@ -0,0 +1,52 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the minimumBribes function below.
+    static void minimumBribes(int[] q) {
+        int swaps = 0;
+        for(int i = 0; i < q.Length; i++)
+        {
+            Console.WriteLine($"i:{i}");
+            int diff = q[i]-1 - i;
+            if(diff > 2)
+            {
+                Console.WriteLine($"Too chaotic");
+                return;
+            }
+            for (int j = Math.Max(0, q[i] - 2); j < i; j++)
+            {
+                if (q[j] > q[i])
+                {
+                    Console.WriteLine($"Swapped:{q[j]} with {q[i]}");
+                    swaps++;
+                }
+            }
+        }
+        Console.WriteLine(swaps);
+    }
+
+    static void Main(string[] args) {
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] q = Array.ConvertAll(Console.ReadLine().Split(' '), qTemp => Convert.ToInt32(qTemp))
+            ;
+            minimumBribes(q);
+        }
+    }
+}
